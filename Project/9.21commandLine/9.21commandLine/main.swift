33b4c2ea6744f0b21d2042646acb72b7cb853697
juuminjun <juuminjun@gmail.com>
2017-09-21 18:27:41 +0900

    a

--- Project/9.21commandLine/9.21commandLine/main.swift
+++ Project/9.21commandLine/9.21commandLine/main.swift
@@ -12,22 +12,12 @@ import Foundation
 
 
 
-var random:[Int] = [1,2,3]
-
-var value:[Int] = [2,5,6]
-
-var ballValue = 0
-var strikeValue = 0
-
-for i in value {
+enum testenum {
     
-    if random.contains(i) {
-        ballValue += 1
-        
-    }else {
-        
-    }
-    
+    case value:Int = 0
+    case stringValue: String = "안녕하세요"
 }
 
-print(ballValue, strikeValue)
+var x:testenum = testenum.
+
+switch
--- Project/reCreateCaladdClosure/Calculator1/UpgradeViewController.swift
+++ Project/reCreateCaladdClosure/Calculator1/UpgradeViewController.swift
@@ -17,12 +17,6 @@ class UpgradeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    /* var operation = ((Double, Double) -> Double)
-    var displayNumber:Double?
-    var inputNumber:String?
-    var digit:Double = 10
-    let operationLabel:[String] = ["/","*","-","+","="] */
-    
     
      //MARK - Secret Method
     let sum = {(num1, num2) -> Double in num1 + num2 }
@@ -47,10 +41,8 @@ class UpgradeViewController: UIViewController {
             
         }
         didSet(oldV){
-            //print("변화 이전의 값은 \(oldV)")
             
         }
-        
     }
     
     
@@ -68,22 +60,48 @@ class UpgradeViewController: UIViewController {
         }
         
 }
-    /*// == 함수
+    // = 함수
     @IBAction func equlAction(_ btn: UIButton) {
         
         if operatorArea == "+" {
-            observers = sum(observers,calculrationArea)
+            calculrationArea = sum(observers,calculrationArea)
+            observers = 0
+            operatorArea = ""
+            disPlayLabel.text = String(calculrationArea)
+            
+            calculrationArea = 0
+            
         }else if operatorArea == "-" {
-            observers = minus(observers,calculrationArea)
+            calculrationArea = minus(calculrationArea, observers) // 큰수가 앞으로 와야해서 위치를 바ㅂ꾸어 준다.
+            observers = 0
+            operatorArea = ""
+            disPlayLabel.text = String(calculrationArea)
+            
+            calculrationArea = 0
+            
         }else if operatorArea == "*" {
-            observers = multi(observers,calculrationArea)
+            calculrationArea = multi(observers,calculrationArea)
+            observers = 0
+            operatorArea = ""
+            disPlayLabel.text = String(calculrationArea)
+            
+            calculrationArea = 0
+            
         }else if operatorArea == "/" {
-            observers = divide(observers,calculrationArea)
+            calculrationArea = divide(observers,calculrationArea)
+            
+            observers = 0
+            operatorArea = ""
+            disPlayLabel.text = String(calculrationArea)
+            
+            calculrationArea = 0
+            
+
         }
         
         
         
-    }*/
+    }
     
     
     // 오퍼레이션 함수
@@ -93,7 +111,7 @@ class UpgradeViewController: UIViewController {
             return disPlayLabel.text = "형 화낸다.."
         }
         // + 연산자 조건 부분.
-        if btn.titleLabel?.text == "+"  {
+        if btn.titleLabel?.text == "+" || btn.titleLabel?.text == "-" || btn.titleLabel?.text == "/" || btn.titleLabel?.text == "*"  {
             
             if operatorArea == "" {
                 operatorArea = (btn.titleLabel?.text)! // 
@@ -129,176 +147,17 @@ class UpgradeViewController: UIViewController {
                 disPlayLabel.text = String(calculrationArea)
                 
             }
-        
-    
-            // 마이너스 연산자 부분
-        }else if btn.titleLabel?.text == "-" {
             
-            if operatorArea == "" {
-                operatorArea = (btn.titleLabel?.text)! //
-                calculrationArea = observers // input 값을 CalArea 에 넣음.
-                observers = 0
-                
-            }else if  operatorArea == "+" {
-                
-                calculrationArea = sum(calculrationArea, observers)
-                observers = 0
-                operatorArea = (btn.titleLabel?.text)!
-                disPlayLabel.text = String(calculrationArea)
-                
-            }else if  operatorArea == "-" {
-                
-                calculrationArea = minus(calculrationArea, observers)
-                observers = 0
-                operatorArea = (btn.titleLabel?.text)!
-                disPlayLabel.text = String(calculrationArea)
-                
-            }else if  operatorArea == "*" {
-                
-                calculrationArea = multi(calculrationArea, observers)
-                observers = 0
-                operatorArea = (btn.titleLabel?.text)!
-                disPlayLabel.text = String(calculrationArea)
-                
-            }else if  operatorArea == "/" {
-                
-                calculrationArea = divide(calculrationArea, observers)
-                observers = 0
-                operatorArea = (btn.titleLabel?.text)!
-                disPlayLabel.text = String(calculrationArea)
-                
-            }
-        
-
-            // 연산자 부분
-        }else if btn.titleLabel?.text == "*" {
-            
-            if operatorArea == "" {
-                operatorArea = (btn.titleLabel?.text)! //
-                calculrationArea = observers // input 값을 CalArea 에 넣음.
-                observers = 0
-                
-            }else if  operatorArea == "+" {
-                
-                calculrationArea = sum(calculrationArea, observers)
-                observers = 0
-                operatorArea = (btn.titleLabel?.text)!
-                disPlayLabel.text = String(calculrationArea)
-                
-            }else if  operatorArea == "-" {
-                
-                calculrationArea = minus(calculrationArea, observers)
-                observers = 0
-                operatorArea = (btn.titleLabel?.text)!
-                disPlayLabel.text = String(calculrationArea)
-                
-            }else if  operatorArea == "*" {
-                
-                calculrationArea = multi(calculrationArea, observers)
-                observers = 0
-                operatorArea = (btn.titleLabel?.text)!
-                disPlayLabel.text = String(calculrationArea)
-                
-            }else if  operatorArea == "/" {
-                
-                calculrationArea = divide(calculrationArea, observers)
-                observers = 0
-                operatorArea = (btn.titleLabel?.text)!
-                disPlayLabel.text = String(calculrationArea)
-                
-            }
-            
-            // '/' 나누기 연산자 부분
-        }else if btn.titleLabel?.text == "/" {
-            
-            if operatorArea == "" {
-                operatorArea = (btn.titleLabel?.text)! //
-                calculrationArea = observers // input 값을 CalArea 에 넣음.
-                observers = 0
-                
-            }else if  operatorArea == "+" {
-                
-                calculrationArea = sum(calculrationArea, observers)
-                observers = 0
-                operatorArea = (btn.titleLabel?.text)!
-                disPlayLabel.text = String(calculrationArea)
-                
-            }else if  operatorArea == "-" {
-                
-                calculrationArea = minus(calculrationArea, observers)
-                observers = 0
-                operatorArea = (btn.titleLabel?.text)!
-                disPlayLabel.text = String(calculrationArea)
-                
-            }else if  operatorArea == "*" {
-                
-                calculrationArea = multi(calculrationArea, observers)
-                observers = 0
-                operatorArea = (btn.titleLabel?.text)!
-                disPlayLabel.text = String(calculrationArea)
-                
-            }else if  operatorArea == "/" {
-                
-                calculrationArea = divide(calculrationArea, observers)
-                observers = 0
-                operatorArea = (btn.titleLabel?.text)!
-                disPlayLabel.text = String(calculrationArea)
-            }
-            
-        }else if btn.titleLabel?.text == "=" {
-            
-            if operatorArea == "" {
-                operatorArea = (btn.titleLabel?.text)! //
-                calculrationArea = observers // input 값을 CalArea 에 넣음.
-                observers = 0
-                
-            }else if  operatorArea == "+" {
-                
-                calculrationArea = sum(calculrationArea, observers)
-                observers = 0
-                operatorArea = (btn.titleLabel?.text)!
-                disPlayLabel.text = String(calculrationArea)
-                
-            }else if  operatorArea == "-" {
-                
-                calculrationArea = minus(calculrationArea, observers)
-                observers = 0
-                operatorArea = (btn.titleLabel?.text)!
-                disPlayLabel.text = String(calculrationArea)
-                
-            }else if  operatorArea == "*" {
-                
-                calculrationArea = multi(calculrationArea, observers)
-                observers = 0
-                operatorArea = (btn.titleLabel?.text)!
-                disPlayLabel.text = String(calculrationArea)
-                
-            }else if  operatorArea == "/" {
-                
-                calculrationArea = divide(calculrationArea, observers)
-                observers = 0
-                operatorArea = (btn.titleLabel?.text)!
-                disPlayLabel.text = String(calculrationArea)
-                
-            }
         }
     }
-
     
-
-        //print(observers,calculrationArea,operatorArea)
-        
-        
-        
-        
-    // 초기화 함수 
+    // 초기화 함수
     @IBAction func resetAction(_ btn: UIButton){
         observers = 0
         calculrationArea = 0
         operatorArea = ""
     }
-    
-    
+
     
 }
 
