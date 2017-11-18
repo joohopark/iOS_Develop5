80b164d4c6a26a617c7d391de59c4267a1f2e473
MinJunJu <devminjunju@gmail.com>
2017-11-18 11:56:17 +0900

    add post

---
+++ Project/17-11-18-firebase/17-11-18-firebase/ViewController.swift
@@ -0,0 +1,36 @@
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  @IBOutlet weak var nameTextField: UITextField!
+  
+  @IBOutlet weak var keytextField: UITextField!
+  @IBOutlet weak var valueTextfield: UITextField!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  @IBAction func insertBtnAction(_ sender: UIButton) {
+  }
+  @IBAction func deleteBtnAction(_ sender: UIButton) {
+  }
+  @IBAction func selectBtnAction(_ sender: UIButton) {
+  }
+  
+  
+  
+  
+  
+  
+
+}
+
