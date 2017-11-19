5c4901cbd6d27f5fdf3b8d964ae35e380927ae2b
MinJunJu <devminjunju@gmail.com>
2017-11-19 22:39:46 +0900

    add Autolayout, kakaotalk UI

---
+++ IOS-AutoLayOut/AutolayOut-Project/17-11-19-KaKaoTalk/17-11-19-KaKaoTalk/ViewController/MyBubbleCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyBubbleCell.swift
+//  17-11-19-KaKaoTalk
+//
+//  Created by MIN JUN JU on 2017. 11. 19..
+//  Copyright © 2017년 MIN JUN JU. All rights reserved.
+//
+
+import UIKit
+
+class MyBubbleCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
