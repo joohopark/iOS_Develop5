5d50994f9d59028650b3e526e70969b68eabb3bf
MinJunJu <devminjunju@gmail.com>
2017-12-30 01:36:25 +0900

    17-12-30-01:36, Following,follower 로직 완성

--- Hekerton/17-11-2-HeackerTon 3/17-11-2-HeackerTon/StartViewController.swift
+++ Hekerton/17-11-2-HeackerTon 3/17-11-2-HeackerTon/StartViewController.swift
@@ -41,6 +41,7 @@ class StartViewController: UIViewController, UITextFieldDelegate {
     textField.resignFirstResponder()
     return true
   }
+    
   override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
     self.view.endEditing(true)
   }
